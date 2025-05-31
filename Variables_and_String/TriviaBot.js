a3057994cbe1b834292212b4dc12b447acc5dae1
pratham karki <prathamkarki21@gmail.com>
2025-05-31 20:39:13 +0545

    variables, reassignment and string concatination practice

---
+++ Variables_and_String/TriviaBot.js
@@ -0,0 +1,26 @@
+// basics variables and string pratice (built trivia bot)
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+let botLocation;
+let favoriteLanguage;
+
+botName = "codeBot";
+botLocation = 'mars';
+favoriteLanguage = 'Javascript';
+
+console.log("My name is " + bot + " and I live on " + botLocation + '.');
+console.log("My favorite programming language is " + favoriteLanguage + ".")
+
+let codingFact;
+codingFact = "Do you know ? " + favoriteLanguage + " is one of the popular programming language.";
+console.log(codingFact);
+
+codingFact = "Did you know ? " + favoriteLanguage + " is a single threaded programming language.";
+console.log(codingFact);
+
+codingFact = "Did you know? " + " Learning " + favoriteLanguage + " takes patience and hardwork.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".")
