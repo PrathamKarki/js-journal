e29d7503cb3dbb44daa0b109635de8f303a8e0da
pratham karki <prathamkarki21@gmail.com>
2025-06-01 16:07:14 +0545

    basic practice session for variables and strings

---
+++ Variables_and_String/SentenceMaker.js
@@ -0,0 +1,39 @@
+//solidifying variables and string 
+
+let adjective;
+let noun;
+let noun2;
+let adjective2;
+let place;
+let verb;
+
+adjective = 'Energetic';
+
+noun = 'ant';
+
+noun2 = 'sugar';
+
+place = 'cave';
+
+adjective2 = 'big';
+
+verb = 'provoked';
+
+let firstStory = "Once upon a time there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " +  firstStory);
+
+//for the second story
+
+let secondStory;
+
+adjective = 'clever';
+noun = 'mouse';
+verb = 'distrubed';
+place = 'woods';
+adjective2 = 'big';
+noun2 = 'cheese';
+
+secondStory = 'Once upon a time there was a(n) ' + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
